1b9e6d4af4df55747521821db3fce64cb807698f
LENGA_TAKAMURA <kotodaman55324678@gmail.com>
2025-05-23 01:56:36 +0900

    [Add]タグ追加　クラス作成

---
+++ Assets/Scripts/InGameSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSystem : MonoBehaviour
+{
+    GameManager _gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.Find("GameManager")?.GetComponent<GameManager>();
+        _gameManager.StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _gameManager.UpdateTimer();
+    }
+}
